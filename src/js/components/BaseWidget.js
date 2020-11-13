e6043ed92165d2b83b1d66e39e666ae9e69da75c
Miszczo <69727417+Miszczo@users.noreply.github.com>
2020-11-13 14:47:58 +0100

    Add baseWidget class and get Value and set Value methods

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -6,4 +6,21 @@ export class BaseWidget{
     thisWidget.dom.wrapper = wrapperElement;
     thisWidget.correctValue = initialValue;
   }
+  get value(){
+    const thisWidget = this;
+
+    return thisWidget.correctValue;
+  }
+
+  set value(assignedValue){
+    const thisWidget = this;
+
+    const newValue = thisWidget.parseValue(assignedValue);
+
+    if(newValue != thisWidget.correctValue && thisWidget.isValid(newValue)){
+      thisWidget.correctValue = newValue;
+      thisWidget.announce();
+    }
+    thisWidget.renderValue();
+  }
 }
