dd5ce6295919f37c27158c816e754dc4ff46548b
Miszczo <69727417+Miszczo@users.noreply.github.com>
2020-11-14 15:20:46 +0100

    start refactoring of AmountWidget class

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,12 +1,13 @@
 import {settings, select} from '../settings.js';
+import {BaseWidget} from './BaseWidget.js';
 
-export class AmountWidget{
-  constructor(element){
+export class AmountWidget extends BaseWidget{
+  constructor(wrapper){
+    super(wrapper, settings.amountWidget.defaultValue);
+
     const thisWidget = this;
 
-    thisWidget.getElements(element);
-    thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(thisWidget.input.value);
+    thisWidget.getElements();
     thisWidget.initActions();
 
 
@@ -14,13 +15,13 @@ export class AmountWidget{
     // console.log('constructor arguments:', element);
   }
 
-  getElements(element){
+  getElements(){
     const thisWidget = this;
 
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
   setValue(value){
@@ -59,14 +60,8 @@ export class AmountWidget{
       console.log('click');
     });
   }
-
-  announce(){
-    const thisWidget = this;
-
-    const event = new CustomEvent('updated', {
-      bubbles: true
-    });
-
-    thisWidget.element.dispatchEvent(event);
+  isValid(newValue){
+    return !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax;
   }
+
 }
